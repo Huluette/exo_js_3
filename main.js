55d228b4235bd678354414d549ad3f18c6751422
Huluette <leana.beuvant@gmail.com>
2023-09-27 15:05:53 +0200

    barre de recherche avec filtre et bouton

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-function getChose() {
-    let liste = document.getElementById("ul").innerHTML;
-    let search = document.getElementById("search").value;
+const barreRecherche = document.getElementById("barreRecherche");
+const boutonRecherche = document.getElementById("boutonRecherche");
+const listeExistante = document.getElementById("listeExistante");
+const resultats = document.getElementById("resultats");
 
-    if(liste!="") {
-        
-    }
-}
+boutonRecherche.addEventListener("click", function() {
+    const valeurRecherche = barreRecherche.value.toLowerCase();
+    
+    // Réinitialiser le style de tous les éléments existants
+    const elementsExistants = Array.from(listeExistante.querySelectorAll("li"));
+    elementsExistants.forEach(function(li) {
+        li.classList.remove("surligne");
+    });
+
+    // Filtrer et surligner les éléments existants qui correspondent à la recherche
+    elementsExistants.forEach(function(li) {
+        const texteLi = li.textContent.toLowerCase();
+
+        if (texteLi.includes(valeurRecherche)) {
+            li.classList.add("surligne");
+        }
+    });
+});
 
 
 
